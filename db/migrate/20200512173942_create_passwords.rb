aad78498278755b0fda5199eb78b7e470f664c9c
mariamfh9 <mfh.haider@gmail.com>
2020-05-12 13:42:55 -0400

    password migration

--- db/migrate/20200512173942_create_passwords.rb
+++ db/migrate/20200512173942_create_passwords.rb
@@ -1,4 +1,10 @@
 class CreatePasswords < ActiveRecord::Migration[5.2]
-  def change
+  def change :passwords do |t|
+    t.string :account_type
+    t.float :account
+    t.integer :value
+    t.integer :owner_id
+    t.integer :account_id
+    end
   end
 end
